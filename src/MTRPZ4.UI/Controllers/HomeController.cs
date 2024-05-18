d0497f1316fb1b278629a730c65f44e5bedb653e
HlibPavlyk <pavlikgo78@gmail.com>
2024-05-18 17:55:20 +0300

    feat: add controllers with views
    - add 3 controllers that represent 3 stages of testing
    - add related to controllers views
    - those elements are prototype and will be modified

--- src/MTRPZ4.UI/Controllers/HomeController.cs
+++ src/MTRPZ4.UI/Controllers/HomeController.cs
@@ -7,23 +7,13 @@ namespace MTRPZ4.UI.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        [Authorize]
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ src/MTRPZ4.UI/Controllers/TestController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MTRPZ4.CoreDomain.Entities;
+using MTRPZ4.Infrastructure;
+
+namespace MTRPZ4.UI.Controllers
+{
+    [Authorize]
+    public class TestController : Controller
+    {
+        private readonly ApplicationDBContext _context;
+        private readonly UserManager<User> _userManager;
+        public TestController(ApplicationDBContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user is null || user.IfCompletedTest)
+            {
+                return RedirectToAction("Index", "Result");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+        public async Task<IActionResult> CompleteTest()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user is { } || !user.IfCompletedTest)
+            {
+                user.IfCompletedTest = true;
+                await _context.SaveChangesAsync();
+            }
+            
+            return RedirectToAction("Index", "Result");
+
+        }
+    }
+}
