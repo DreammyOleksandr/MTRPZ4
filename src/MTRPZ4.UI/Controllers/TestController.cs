840930d85ad57f58326d9c1e85019ddb3fd72565
Nazg0r <gorchinshiy83@gmail.com>
2024-05-30 19:22:59 +0300

    feat: update Test controller methods
    -add model transfer to Get method
    -add method for result saving

--- src/MTRPZ4.UI/Controllers/TestController.cs
+++ src/MTRPZ4.UI/Controllers/TestController.cs
@@ -1,20 +1,24 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Azure;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MTRPZ4.Application.DTO;
+using MTRPZ4.Application.Services;
 using MTRPZ4.CoreDomain.Entities;
 using MTRPZ4.Infrastructure;
+using MTRPZ4.Infrastructure.Repository.IRepository;
 
 namespace MTRPZ4.UI.Controllers
 {
     [Authorize]
     public class TestController : Controller
     {
-        private readonly ApplicationDBContext _context;
         private readonly UserManager<User> _userManager;
-        public TestController(ApplicationDBContext context, UserManager<User> userManager)
+		private readonly ICardService _cardService;
+		public TestController(UserManager<User> userManager, ICardService cardService)
         {
-            _context = context;
             _userManager = userManager;
+            _cardService = cardService;
         }
         public async Task<IActionResult> Index()
         {
@@ -26,22 +30,32 @@ namespace MTRPZ4.UI.Controllers
             }
             else
             {
-                return View();
+                IEnumerable<CardDTO> cards = await _cardService.GetRandomCards();
+				List<CardDTO> cardsList = cards.ToList();
+
+				return View(cardsList);
             }
-
         }
-        public async Task<IActionResult> CompleteTest()
-        {
-            var user = await _userManager.GetUserAsync(User);
 
-            if (user is { } || !user.IfCompletedTest)
-            {
-                user.IfCompletedTest = true;
-                await _context.SaveChangesAsync();
-            }
-            
-            return RedirectToAction("Index", "Result");
+        [Route("Test/{ColorId}/{FontId}/{PriceId}")]
+		public async Task<IActionResult> Index(ChosedCardDTO? choice)
+		{
+			var user = await _userManager.GetUserAsync(User);
 
-        }
+			if (user is { } || !user.IfCompletedTest)
+			{
+				user.IfCompletedTest = true;
+			}
+
+			try
+			{
+				await _cardService.SaveCardChoice(choice);
+				return RedirectToAction("Index", "Result");
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
     }
 }
