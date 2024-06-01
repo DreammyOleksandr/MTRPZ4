f750214f2ae067449e982ae745bc1061cfa4b984
HlibPavlyk <pavlikgo78@gmail.com>
2024-06-01 17:56:36 +0300

    feat: add startup data
    - edit db seeding methods

--- src/MTRPZ4.Infrastructure/Seeding/ButtonColorsSeeder.cs
+++ src/MTRPZ4.Infrastructure/Seeding/ButtonColorsSeeder.cs
@@ -10,26 +10,28 @@ namespace MTRPZ4.Infrastructure.Seeding
 {
     public static class ButtonSeeder
     {
+        private static Random random = new Random();
          public static List<Color> GetButtonColors() => new List<Color>
         {
-            new Color { Id = 1, Pigment = "Red", Count = 26,},
-            new Color { Id = 2, Pigment = "Orange", Count = 13,},
-            new Color { Id = 3, Pigment = "Blue", Count = 7,}
+            new Color { Id = 1, Pigment = "Red", Count = GetRandomCount(),},
+            new Color { Id = 2, Pigment = "Orange", Count = GetRandomCount(),},
+            new Color { Id = 3, Pigment = "Blue", Count = GetRandomCount(),}
         };
 
         public static List<Price> GetButtonPrices() => new List<Price>
         {
-            new Price { Id = 1, Value = 12.49M, Count = 25,},
-            new Price { Id = 2, Value = 22.78M, Count = 12,},
-            new Price { Id = 3, Value = 33.11M, Count = 18,},
+            new Price { Id = 1, Value = 12.49M, Count = GetRandomCount(),},
+            new Price { Id = 2, Value = 22.78M, Count = GetRandomCount(),},
+            new Price { Id = 3, Value = 33.11M, Count = GetRandomCount(),},
         };
         
         public static List<Font> GetButtonFonts() => new List<Font>
         {
-            new Font { Id = 1, Type = "Arial", Count = 11,},
-            new Font { Id = 2, Type = "Times New Roman", Count = 20,},
-            new Font { Id = 3, Type = "Comic Sans", Count = 12,}
+            new Font { Id = 1, Type = "Arial", Count = GetRandomCount(),},
+            new Font { Id = 2, Type = "Times New Roman", Count = GetRandomCount(),},
+            new Font { Id = 3, Type = "Comic Sans", Count = GetRandomCount(),}
         };
+        private static int GetRandomCount() => random.Next(10, 30);
 
 
     }
